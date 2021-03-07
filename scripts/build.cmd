94ef375648ca158a56bdd0de5fb48a481bad4d40
luolingchun <luolingchun@outlook.com>
2021-03-07 21:15:03 +0800

    add qgis plugins

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,7 +1,9 @@
 :: -w 窗口程序 -c 控制台程序
+set QGIS_VERSION=3.16
 "C:\Program Files\QGIS 3.16\bin\python-qgis-ltr.bat" -m PyInstaller -c ^
---add-data="C:\Program Files\QGIS 3.16\apps\Python37\Lib\site-packages\PyQt5\*.pyd;PyQt5" ^
---add-data="C:\Program Files\QGIS 3.16\share;share" ^
+--add-data="C:\Program Files\QGIS %QGIS_VERSION%\apps\Python37\Lib\site-packages\PyQt5\*.pyd;PyQt5" ^
+--add-data="C:\Program Files\QGIS %QGIS_VERSION%\apps\qgis-ltr\plugins;qgis\plugins" ^
+--add-data="C:\Program Files\QGIS %QGIS_VERSION%\share;share" ^
 --add-data="data;data" ^
 --add-data="images;images" ^
 --icon=images\pyqgis.ico ^
