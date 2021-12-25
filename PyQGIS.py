7c0f3ca98d3c40fe1f5c097cfb1bb4b164380860
luolingchun <luolingchun@outlook.com>
2021-12-25 14:57:07 +0800

    加载翻译文件

--- PyQGIS.py
+++ PyQGIS.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 # @Author  : llc
 # @Time    : 2020/4/19 16:23
-import os
 
+from qgis.PyQt import QtCore
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.core import QgsApplication
-# from qgis.utils import iface
 
 from config import setup_env
 from splash import NewSplashScreen
-from utils.plugins import loadPlugins
-from utils.interface import initInterface
+# from utils.plugins import loadPlugins
+# from utils.interface import initInterface
 from widgets.mainWindow import MainWindow
 
+# from qgis.utils import iface
+
 # 添加插件目录
-os.environ["QGIS_PLUGINPATH"] = r"D:\workspace\QGIS\plugins"
+# os.environ["QGIS_PLUGINPATH"] = r"D:\workspace\QGIS\plugins"
 
 setup_env()
 # 适应高分辨率
@@ -24,6 +25,11 @@ QgsApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 QgsApplication.setStyle("Fusion")
 # 创建对QgsApplication的引用，第二个参数设置为False将禁用GUI
 qgs = QgsApplication([], True)
+
+t = QtCore.QTranslator()  # 先新建一个 QTranslator
+# 加载翻译文件
+t.load(r'C:\Program Files\QGIS 3.16\apps\qgis-ltr\i18n\qgis_zh-Hans.qm')
+qgs.installTranslator(t)
 
 # 启动画面
 splash = NewSplashScreen()
@@ -36,9 +42,9 @@ qgs.initQgis()
 # 主窗口
 mainWindow = MainWindow()
 # 初始化iface
-iface = initInterface(mainWindow)
+# iface = initInterface(mainWindow)
 # 加载插件
-loadPlugins(iface)
+# loadPlugins(iface)
 # 设置图标
 mainWindow.setWindowIcon(QIcon('./images/pyqgis.png'))
 splash.finish(mainWindow)
